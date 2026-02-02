0dc276f49aa96ef63902c6d68387ee00594ac65e
y-jh107 <jin99545@naver.com>
2026-02-02 12:23:39 +0900

    [Feat] 키워드 기반 검색 API 구현

--- netflix_clone/src/api/contentsApi.ts
+++ netflix_clone/src/api/contentsApi.ts
@@ -1,6 +1,17 @@
 import type { Content } from '../data/content';
 import { contents } from '../data/content';
 
+/* 검색 결과 인터페이스 */
+export interface SearchResult {
+  exactMatches: Content[];
+  relatedMatches: Content[];
+}
+
+/**
+ * 모든 API는 0.3초의 딜레이 시간이 있음
+ * Backend에서 데이터를 불러오는 것 같은 효과
+ */
+
 /* 컨텐츠 불러오기 API */
 export const getContents = async (): Promise<Content[]> => {
   return new Promise((resolve) => {
@@ -18,6 +29,36 @@ export const getContentById = async (id: number): Promise<Content> => {
         return;
       }
       resolve(found);
+    }, 300);
+  });
+};
+
+/* 키워드로 컨텐츠 검색하기 API */
+export const searchContents = async (
+  keyword: string,
+): Promise<SearchResult> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      /* 키워드를 기반으로 제목, 출연, 제작에서 탐색 */
+      const exactMatches = contents.filter((c) => {
+        c.title.includes(keyword) ||
+          c.cast.includes(keyword) ||
+          c.creator.includes(keyword);
+      });
+
+      /* 유사 콘텐츠 추가(중복은 제거) */
+      const relatedMatches = contents.filter((c) => {
+        if (exactMatches.includes(c)) return false;
+
+        return exactMatches.some(
+          (m) =>
+            m.genre.some((g) => c.genre.includes(g)) ||
+            m.features.some((f) => c.features.includes(f)) ||
+            c.type === m.type,
+        );
+      });
+
+      resolve({ exactMatches, relatedMatches });
     }, 300);
   });
 };
