cf6e0fd2dbda16d47607c01f76706ed23ec2c8d9
양진혁 <jin99545@gmail.com>
2026-02-06 11:09:13 +0900

    [Feat] 컨텐츠 카드 라우팅 완료

--- netflix_clone/src/components/MovieList/MovieCard.tsx
+++ netflix_clone/src/components/MovieList/MovieCard.tsx
@@ -1,28 +1,43 @@
 // 기본 카드 컴포넌트
 
-import type { MovieCardProps } from "./types";
+import type { MovieCardProps } from './types';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-export default function MovieCard({ movie, onHover, onLeave, cardWidth }: MovieCardProps) {
-    //마우스 진입 시 카드 위치를 계산하여 부모에게 전달
-    const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        onHover?.(movie, rect);
-    }
+export default function MovieCard({
+  movie,
+  onHover,
+  onLeave,
+  cardWidth,
+}: MovieCardProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  //마우스 진입 시 카드 위치를 계산하여 부모에게 전달
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    onHover?.(movie, rect);
+  };
 
-    return (
-        <div
-            className="relative flex-shrink-0 w-[230px] cursor-pointer
+  const handleClick = () => {
+    navigate(`/detail/${movie.id}`, {
+      state: { background: location },
+    });
+  };
+
+  return (
+    <div
+      className='relative flex-shrink-0 w-[230px] cursor-pointer
             transition-transform duration-300 ease-out
-            hover:z-10"
-            style={{width: cardWidth ? `${cardWidth}px`: '230px'}}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={onLeave}
-        >
-            <img
-                src={movie.backdropUrl}
-                alt={movie.title}
-                className="w-full aspect-video object-cover rounded-[.2vw]"
-            />
-        </div>
-    );
+            hover:z-10'
+      style={{ width: cardWidth ? `${cardWidth}px` : '230px' }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={onLeave}
+      onClick={handleClick}
+    >
+      <img
+        src={movie.backdropUrl}
+        alt={movie.title}
+        className='w-full aspect-video object-cover rounded-[.2vw]'
+      />
+    </div>
+  );
 }
