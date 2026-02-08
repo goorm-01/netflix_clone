78ea456b8985dfd50d485352a00c36a3bfe8327a
lee0806 <leeseh0806@gmail.com>
2026-02-08 16:49:52 +0900

    마이 페이지 호버 메뉴 생성

--- netflix_clone/src/components/Header/Header.tsx
+++ netflix_clone/src/components/Header/Header.tsx
@@ -15,6 +15,9 @@ export default function Header() {
   // 호버 메뉴
   const [isHoverMenu, setIsHoverMenu] = useState<boolean>(false);
 
+  // 마이 페이지 호버
+  const [isHoverMyPage, setIsHoverMyPage] = useState<boolean>(false);
+
   const navigate = useNavigate();
 
   const {
@@ -236,23 +239,131 @@ export default function Header() {
             </svg>
           </button>
 
-          <button className='flex items-center columns-1'>
-            <img
-              src='./src/assets/Header/profile.png'
-              className='w-[32px] h-[32px] rounded-[4px]'
-              alt='Profile'
-            />
-            <div className='ml-[10px]'>
-              <svg viewBox='0 0 16 16' width='16' height='16'>
-                <path
-                  fill='currentColor'
-                  fillRule='evenodd'
-                  d='M11.6 6.5c.15 0 .22.18.12.28l-3.48 3.48a.33.33 0 0 1-.48 0L4.28 6.78a.17.17 0 0 1 .12-.28z'
-                  clipRule='evenodd'
-                ></path>
-              </svg>
-            </div>
-          </button>
+          <div className='relative'>
+            <button
+              className='flex items-center columns-1'
+              onMouseEnter={() => setIsHoverMyPage(true)}
+              onMouseLeave={() => setIsHoverMyPage(false)}
+            >
+              <img
+                src='./src/assets/Header/profile.png'
+                className='w-[32px] h-[32px] rounded-[4px]'
+                alt='Profile'
+              />
+              <div className='ml-[10px]'>
+                <svg viewBox='0 0 16 16' width='16' height='16'>
+                  <path
+                    fill='currentColor'
+                    fillRule='evenodd'
+                    d='M11.6 6.5c.15 0 .22.18.12.28l-3.48 3.48a.33.33 0 0 1-.48 0L4.28 6.78a.17.17 0 0 1 .12-.28z'
+                    clipRule='evenodd'
+                  ></path>
+                </svg>
+              </div>
+            </button>
+
+            {isHoverMyPage && (
+              <div
+                onMouseEnter={() => setIsHoverMyPage(true)}
+                onMouseLeave={() => setIsHoverMyPage(false)}
+              >
+                {/* 호버 브릿지 */}
+                <div className='absolute top-[30px] right-0 w-full h-[30px]' />
+
+                {/* 드롭다운 메뉴 */}
+                <div className='absolute top-[50px] right-[10px] bg-[rgba(20,20,20,0.95)] border border-gray-700 min-w-[200px] py-2'>
+                  {/* 키즈 아이콘 */}
+                  <div className='flex items-center gap-3 px-3 py-2 hover:bg-gray-800 cursor-pointer'>
+                    <img
+                      className='profile-icon'
+                      src='https://occ-0-3682-988.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABdTeFDWGGsM48NmQvPPwI9VDJ_aeLDk_7U5sowYemKcU_IW57cQR5Vn1fJU8F2tlp9Atv3V13C6rQ4-AT88O_8asZcow4xY.png?r=15b'
+                      alt=''
+                    ></img>
+                    <span className='text-white text-[12px]'>키즈</span>
+                  </div>
+
+                  {/* 메뉴 */}
+                  <div className='flex items-center gap-3 px-3 py-2 hover:bg-gray-800 cursor-pointer'>
+                    <svg width='24' height='24' viewBox='0 0 24 24' fill='none'>
+                      <path
+                        fill='currentColor'
+                        fillRule='evenodd'
+                        d='M19.121 1.707a3 3 0 0 0-4.242 0l-1.586 1.586-.707.707-11 11A2 2 0 0 0 1 16.414V21a2 2 0 0 0 2 2h4.586A2 2 0 0 0 9 22.414l11-11 .707-.707 1.586-1.586a3 3 0 0 0 0-4.242zM15.586 7 14 5.414l-11 11V19a2 2 0 0 1 2 2h2.586l11-11L17 8.414 6.707 18.707l-1.414-1.414zm.707-3.879a1 1 0 0 1 1.414 0l3.172 3.172a1 1 0 0 1 0 1.414L20 8.586 15.414 4z'
+                        clipRule='evenodd'
+                      ></path>
+                    </svg>
+                    <span className='text-white text-[12px]'>프로필 관리</span>
+                  </div>
+
+                  <div className='flex items-center gap-3 px-3 py-2 hover:bg-gray-800 cursor-pointer'>
+                    <svg
+                      viewBox='0 0 24 24'
+                      width='24'
+                      height='24'
+                      data-icon='ProfileArrowMedium'
+                      data-icon-id=':r35:'
+                      aria-hidden='true'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      role='img'
+                    >
+                      <path
+                        fill='currentColor'
+                        fillRule='evenodd'
+                        d='M6 1a4 4 0 0 0-4 4v12a4 4 0 0 0 4 4h3.59l-1.3 1.3 1.42 1.4 3-3a1 1 0 0 0 0-1.4l-3-3-1.42 1.4L9.6 19H6a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-3v2h3a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4zm1.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M18 8.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1.6 3.7a5 5 0 0 1-2.9.8 5 5 0 0 1-2.9-.8l-1.2 1.6a7 7 0 0 0 4.1 1.2c1.58 0 3.07-.43 4.1-1.2z'
+                        clipRule='evenodd'
+                      ></path>
+                    </svg>
+                    <span className='text-white text-[12px]'>프로필 이전</span>
+                  </div>
+
+                  <div className='flex items-center gap-3 px-3 py-2 hover:bg-gray-800 cursor-pointer'>
+                    <svg
+                      width='24'
+                      height='24'
+                      viewBox='0 0 24 24'
+                      fill='currentColor'
+                    >
+                      <path
+                        fill='currentColor'
+                        fillRule='evenodd'
+                        d='M15 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m2 0A5 5 0 1 1 7 5a5 5 0 0 1 10 0M4 21a8 8 0 1 1 16 0v.514A68 68 0 0 1 12 22a68 68 0 0 1-8-.486zm17.15 2.378-.15-.99.151.99a1 1 0 0 0 .849-.99V21c0-5.523-4.477-10-10-10S2 15.477 2 21v1.389a1 1 0 0 0 .849.988L3 22.39c-.151.988-.15.988-.15.989h.003l.01.002.038.005.142.02q.186.027.535.072A70 70 0 0 0 12 24a70 70 0 0 0 8.422-.523q.35-.045.535-.072l.142-.02.038-.005.01-.002z'
+                        clipRule='evenodd'
+                      ></path>
+                    </svg>
+                    <span className='text-white text-[12px]'>계정</span>
+                  </div>
+
+                  <div className='flex items-center gap-3 px-3 py-2 hover:bg-gray-800 cursor-pointer'>
+                    <svg
+                      width='24'
+                      height='24'
+                      viewBox='0 0 24 24'
+                      fill='currentColor'
+                    >
+                      <path
+                        fill='currentColor'
+                        fillRule='evenodd'
+                        d='M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0m0 8c-1.317 0-2 .743-2 1.5H8C8 7.257 10.003 6 12 6s4 1.257 4 3.5c0 1.349-1.08 2.268-2.178 2.68-.265.1-.49.25-.636.411-.14.156-.186.292-.186.409v1h-2v-1c0-1.435 1.168-2.335 2.119-2.692.729-.274.881-.66.881-.808 0-.757-.683-1.5-2-1.5m1.5 8.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0'
+                        clipRule='evenodd'
+                      ></path>
+                    </svg>
+                    <span className='text-white text-[12px]'>고객 센터</span>
+                  </div>
+
+                  {/* 언더라인 */}
+                  <div className='border-t border-gray-700 my-2'></div>
+
+                  {/* 로그아웃 */}
+                  <div className='px-3 py-2 text-center hover:bg-gray-800 cursor-pointer'>
+                    <span className='text-white text-[12px]'>
+                      넷플릭스에서 로그아웃
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
